87217a4c615a68831ad988ab389a3626c77122c0
Ishika Soni <ishikasoni50@gmail.com>
2021-04-18 17:24:11 -0400

    added login attributes and coco touch class

---
+++ Focus-FM/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  Focus-FM
+//
+//  Created by Ishika Soni  on 4/18/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var UsernameTextField: UITextField!
+    
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
+    
+    @IBAction func SignInButton(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func SignUpButton(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
