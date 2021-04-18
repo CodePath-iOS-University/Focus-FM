8bd0d61032923dc06a85ff0ab7acc351fe047cf3
eric_xiao <xiao.yuan0217@gmail.com>
2021-04-18 18:42:01 -0400

    ParseConfig completed, changed password fields to private.

--- Focus-FM/AppDelegate.swift
+++ Focus-FM/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "0bpluuz8qrrgd8bWOmkkVNlrOfpkF70NEkVkpkS6"
+                    $0.clientKey = "I8QwCjlKhSow5oaDZ6xkrVAR6vVPApa9eUWlRldL"
+                    $0.server = "https://parseapi.back4app.com/"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
